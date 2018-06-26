1d605f5d9f51e8d3808a8fec198704fb0eaf2cd4
Susan Longley <susanlongley@PEG-MAC-007209.local>
2018-06-26 14:36:28 +0100

    challenge 8 complete

---
+++ Challenge_8.rb
@@ -0,0 +1,9 @@
+default = "clap your hands"
+special = "and you really want to show it"
+
+def song(input1, input2)
+  puts "If you're happy and you know it #{input1},
+  if you're happy and you know it, #{input2},
+  if you're happy and you know it #{input1}."
+end
+song(default, special)
