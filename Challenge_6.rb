24801d3ff53030819738fb8e5bf1f252ece2c80f
Susan Longley <susanlongley@PEG-MAC-007209.local>
2018-06-26 14:27:34 +0100

    challenge 6 complete

---
+++ Challenge_6.rb
@@ -0,0 +1,13 @@
+
+
+def numbers(number)
+  if number >= 1
+    puts "Your number is a positive number"
+  elsif number <= -1
+    puts "Your number is a negative number "
+  else
+    puts "Your number is neither negative or positive"
+  end
+end
+
+numbers(0)
